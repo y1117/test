762d8360744369815db55c03dc10e2437676ba69
Yunsub <sim861117@gmail.com>
2024-05-16 16:37:31 +0900

    [feature] p33

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,11 @@
 ﻿#include "pch.h"
 #include "../test/prime-factors.cpp"
 
+#include <vector>
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-	PrimeFactor pf;
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
--- test/prime-factors.cpp
+++ test/prime-factors.cpp
@@ -1,3 +1,9 @@
-﻿class PrimeFactor {
+﻿#include <vector>
+using namespace std;
 
+class PrimeFactor {
+public:
+	vector <int> of(int i) {
+		return {};
+	}
 };
