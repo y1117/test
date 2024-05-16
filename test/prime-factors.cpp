d579ec447aae5c90d15a2203f00181186747f22a
Yunsub <sim861117@gmail.com>
2024-05-16 17:04:22 +0900

    [refactor] p59

--- test/prime-factors.cpp
+++ test/prime-factors.cpp
@@ -5,18 +5,11 @@ class PrimeFactor {
 public:
 	vector <int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-			else {
-				result.push_back(number);
+
+		for (int divisor = 2; number > 1; divisor++) {
+			while (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
 			}
 		}
 		return result;
